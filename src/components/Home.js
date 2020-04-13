6cbee551871b672c7fefce83a8c7ad5194789bf2
sumit gupta <gsumitee@gmail.com>
2020-04-13 12:42:21 -0400

    commit to test CLI

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,31 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello Home Page</h1>
+        <br />
+        <div
+          class="card text-white bg-primary mb-3 d-inline-block"
+          style={{ "max-width": "20rem;" }}
+        >
+          <div class="card-header">My personal Webpages</div>
+          <div class="card-body">
+            <h4 class="card-title"></h4>
+            <p class="card-text">
+              This site was build using Create React App
+              (https://facebook.github.io/create-react-app/). <br />
+              The code is hosted on gitlab. It uses AWS Amplify to host the UI
+              on AWS. <br />
+              Amplify is used to build scalable cloud-powered apps. It also can
+              deploy and host web apps. <br />
+              For now I am using only the deploy feature of my github UI code. I
+              connected my git repository to continuously deploy from changes.{" "}
+              <br />
+              > npm install -g @aws-amplify/cli <br />
+              > amplify configure <br />
+              > amplify init <br />> npm install --save aws-amplify <br />> npm
+              install --save aws-amplify-react
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
